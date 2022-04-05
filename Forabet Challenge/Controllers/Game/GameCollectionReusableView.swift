b905e5ac7547753d787626980efb65b84ecaeba5
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-05 09:57:02 +0200

    added GameCollectionReusableView.swift

---
+++ Forabet Challenge/Controllers/Game/GameCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  GameCollectionReusableView.swift
+//  Forabet Challenge
+//
+//  Created by BSergio on 05.04.2022.
+//
+
+import UIKit
+
+class GameCollectionReusableView: UICollectionReusableView {
+    static let id = "GameCollectionReusableView"
+    
+    
+}
