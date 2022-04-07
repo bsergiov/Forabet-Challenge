96324f3ed11e4522f79ea6b400efa13e4222c15f
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-07 11:39:05 +0200

    added GameControllCollectionViewCell.swift

---
+++ Forabet Challenge/Controllers/Game/Cell/GameControllCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  GameControllCollectionViewCell.swift
+//  Forabet Challenge
+//
+//  Created by SV on 07.04.2022.
+//
+
+import UIKit
+
+class GameControllCollectionViewCell: UICollectionViewCell {
+    
+    static let id = "GameControllCollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
