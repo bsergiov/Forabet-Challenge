e16730005290bca7bbd2998a2eb4226064aadf3f
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-04-13 17:09:03 +0200

    WorkTargetModel.swift added

---
+++ Forabet Challenge/Models/WorkTargetModel.swift
@@ -0,0 +1,15 @@
+//
+//  WorkTargetModel.swift
+//  Forabet Challenge
+//
+//  Created by SV on 01.04.2022.
+//
+
+import Foundation
+
+struct WorkTargetModel {
+    let host: String
+    let path: String
+    let parametrs: String
+    let statusButton: Bool
+}
